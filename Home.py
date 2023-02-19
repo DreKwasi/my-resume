51a87c6f838e474a1246757e2c8008263380e771
DreKwasi <andrewsboateng137@gmail.com>
2023-02-19 22:07:39 +0000

    Option Menu Bug

--- Home.py
+++ Home.py
@@ -2,7 +2,6 @@ import streamlit as st
 from helper import styles
 from pathlib import Path
 from PIL import Image
-from streamlit_option_menu import option_menu
 from streamlit_extras.badges import badge
 
 
